f0db4365212b527e7c86f71e4a75ac5a021ecd85
lucassingh <lucas.singh10@gmail.com>
2025-10-23 16:04:08 -0300

    fix build

--- index.ts
+++
@@ -1,7 +0,0 @@
-// Exporta tu componente Button
-export { Siglo21ButtonComponent as Siglo21Button } from './src/components/Button/Siglo21ButtonComponent';
-export type { Siglo21ButtonComponentProps } from './src/components/Button/Siglo21ButtonComponent';
-
-// Exporta tus temas
-export { default as siglo21Theme } from './config/theme'
-export { darkTheme } from './config/theme';
